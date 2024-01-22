7c7dae2496f4eff9a456b375bdf43fe77dd3abb8
Terry Lao <terry.lao@gmail.com>
2024-01-22 20:49:28 +0800

    Update OTPGenerator.cc
    add key parameter

--- OTPGenerator.cc
+++ OTPGenerator.cc
@@ -10,15 +10,18 @@
 #include <windows.h>
 
 int main(int argc, char **argv) {
-    fprintf(stderr, "OTP generator version: 1.0\n");
-    char ipt1[41],ipt2[41],ipt3[41];
-    char userinput[7];
-    memset(&ipt1[0],0,41);
-    memset(&ipt2[0],0,41);
-    memset(&ipt3[0],0,41);
-    memset(&userinput,0,7);
-    genOTP(&ipt1[0],&ipt2[0],&ipt3[0]);
-    memcpy(userinput,ipt1,6);
-    printf("OTP:%s\n",userinput);
-    return 0;
+  fprintf(stderr, "OTP generator version: 1.2\n");
+	if (argc==0){
+		fprintf(stderr, "OTPGenerator [key]\n");
+	}
+  char ipt1[41],ipt2[41],ipt3[41];
+  char userinput[7];
+  memset(&ipt1[0],0,41);
+  memset(&ipt2[0],0,41);
+  memset(&ipt3[0],0,41);
+  memset(&userinput,0,7);
+  genOTP(&ipt1[0],&ipt2[0],&ipt3[0],argv[1]);
+  memcpy(userinput,ipt1,6);
+  printf("OTP with %s :%s\n",argv[1],userinput);
+  return 0;
 }
